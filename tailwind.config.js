e8c2d021e0abc7ef61cdff02c5fa7c3aabd8c50c
David Marcano <dam64@njit.edu>
2026-01-27 19:13:40 -0500

    installing tailwind, adding color palette theme

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'soft-white': '#F0F3EF',
+        'soft-green': '#869C90',
+        'dark-grey': '#727576',
+        'light-grey': '#8A8A8B',
+        'dark-blue': '#1C242B',
+      },
+    },
+  },
+  plugins: [],
+}
